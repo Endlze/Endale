c1a1197223f16160cf4ec301d39e9b194810cf8f
Endlze <alebel768432@gmail.com>
2024-06-02 17:55:36 -0700

    Add files via upload

---
+++ PORTFOLIO/JS/script.js
@@ -0,0 +1,7 @@
+/* =========================== typing animation ==================*/
+var typed = new Typed(".typing", {
+    strings:["" ,"Software Learner " ,"Web Learner","Graphic Designer","Youtuber","Tiktoker"],
+    typespead:100,
+    backspeed:60,
+    loop:true
+})
